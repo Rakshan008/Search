134c16e073aa2c87d4869d3bcbcfee801622cfef
Rakshan008 <142168584+Rakshan008@users.noreply.github.com>
2025-04-24 16:08:52 +0530

    Create postcss.config.js

---
+++ frontend/postcss.config.js
@@ -0,0 +1,6 @@
+export default {
+  plugins: {
+    tailwindcss: {}, // Enables Tailwind CSS processing
+    autoprefixer: {}, // Adds vendor prefixes (like -webkit-, -moz-) for CSS properties
+  },
+}
